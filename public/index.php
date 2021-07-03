59f2128301832b4b224a39ae6df458e3308673f3
ben lim <lim6112j@gmail.com>
2021-07-04 00:26:20 +0900

    apache prettyurl, custom routes made

--- public/index.php
+++ public/index.php
@@ -1,11 +1,15 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
-use app\core;
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
 use app\core\Application;
 
-echo "hello world";
 $app = new Application();
 $app->router->get('/', function(){
     return "Hello World";
+});
+$app->router->get('/contact', function(){
+    return "contact";
 });
 $app->run();
