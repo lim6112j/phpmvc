6c931165239e64c4055a47fdb3166bf74e4c4ced
lim6112j <lim6112j@gmail.com>
2021-07-25 22:16:50 +0900

    code edited for vue search test

--- src/App.vue
+++ src/App.vue
@@ -7,44 +7,64 @@
     <input v-model="question" />
   </p>
   <p>{{ answer }}</p>
+  <p>
+    <input v-model="search"/>
+  </p>
+  <p> {{ searchTerm  }} </p>
 </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios'
-
+import _ from 'lodash'
 export default {
-  name: 'App',
-  components: {
+     name: 'App',
+     components: {
     // HelloWorld
-  },
-    data() {
-      return {
-        question: '',
-        answer: 'Questions usually contain a question mark. ;-)'
-      }
-    },
-    watch: {
+     },
+
+     data() {
+         return {
+             question: '',
+             answer: 'Questions usually contain a question mark. ;-)',
+             search: '',
+             searchTerm: '문자를 입력하세요.'
+         }
+     },
+     watch: {
       // whenever question changes, this function will run
-      question(newQuestion) {
-        if (newQuestion.indexOf('?') > -1) {
-          this.getAnswer()
-        }
+         question(newQuestion) {
+             if (newQuestion.indexOf('?') > -1) {
+                 this.getAnswer()
+         }
+      },
+      search: function () {
+          this.debounceSearch()
       }
-    },
-    methods: {
-      getAnswer() {
-        this.answer = 'Thinking...'
-        axios
-          .get('https://yesno.wtf/api')
-          .then(response => {
-            this.answer = response.data.answer
-          })
-          .catch(error => {
-            this.answer = 'Error! Could not reach the API. ' + error
-          })
-      }
+     },
+     created: function (){
+      this.debounceSearch = _.debounce(this.getSearch, 500)
+     },
+     methods: {
+         getSearch: function() {
+             if(this.search.length === 0){
+                 this.searchTerm = '문자를 입력하세요.'
+                 return
+             }
+             this.searchTerm = this.search
+         },
+         getAnswer() {
+             this.answer = 'Thinking...'
+             axios
+             .get('https://yesno.wtf/api')
+             .then(response => {
+                 this.answer = response.data.answer
+             })
+             .catch(error => {
+                 this.answer = 'Error! Could not reach the API. ' + error
+             })
+         }
     }
 }
 </script>
